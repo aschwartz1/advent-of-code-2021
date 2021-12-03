b3e52415a98b03d9610cc04a06ef821f6a3b9da0
Alex Schwartz <aschwartz@detaso.com>
2021-12-03 09:02:07 -0600

    Add Day2 spec and ruby file

---
+++ lib/day_2.rb
@@ -0,0 +1,7 @@
+class Day2
+  class << self
+    def hi
+      "vanakkam"
+    end
+  end
+end
---
+++ spec/day_2_spec.rb
@@ -0,0 +1,7 @@
+require "./lib/day_2"
+
+describe Day2 do
+  it "works" do
+    expect(Day2.hi).to eq("vanakkam")
+  end
+end
