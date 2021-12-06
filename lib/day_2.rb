274c73ca53e59cdbe85e16241819da5072368a53
Alex Schwartz <aschwartz@detaso.com>
2021-12-05 21:05:58 -0600

    Solve day 2 puzzle

--- lib/day_2.rb
+++ lib/day_2.rb
@@ -1,7 +1,32 @@
 class Day2
+  def self.run(input)
+    instructions = parse(input)
+    totals = {horizontal: 0, depth: 0}
+
+    instructions.each do |movement|
+      command = movement.keys.first
+
+      case command
+      when "forward"
+        totals[:horizontal] += movement[command]
+      when "up"
+        totals[:depth] -= movement[command]
+      when "down"
+        totals[:depth] += movement[command]
+      end
+    end
+
+    totals
+  end
+
   class << self
-    def hi
-      "vanakkam"
+    private
+
+    def parse(input)
+      input.map do |line|
+        split = line.split
+        {split[0] => split[1].to_i}
+      end
     end
   end
 end
