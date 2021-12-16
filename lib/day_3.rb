58d99e39e9e958d642e2da69e14f20e259d1a26e
Alex Schwartz <aschwartz@detaso.com>
2021-12-16 09:30:48 -0600

    Implement logic for finding most/least common binary
    Techinally 'wip' for part 2. I left a TODO comment in `part2` for where
    to pick up.
    
    Lots of code and trial & error went into this, and I know there is room
    for drying up the most/least recursive methods. Right now I'm out of
    time and brainpower to continue, though.

--- lib/day_3.rb
+++ lib/day_3.rb
@@ -15,6 +15,16 @@ class Day3
     multiply_rates
   end
 
+  def part2
+    binaries_array = @binaries.map { |binary_string| binary_string.chars.map(&:to_i) }
+
+    oxygen_generator_binary = calculate_oxygen_generator(binaries_array)
+    co2_scrubber_binary = calculate_co2_scrubber(binaries_array)
+
+    # TODO: turn them into ints, multiply together & return
+    #   - will need to generalize the multiply_rates method so I can reuse it
+  end
+
   private
 
   def add_binaries
@@ -53,9 +63,59 @@ class Day3
 
   def read_input(filepath)
     File.readlines(filepath, chomp: true)
+  end
 
-    # File.open(filepath).map do |line|
-    #   parse(line)
-    # end
+  def calculate_oxygen_generator(binaries_array)
+    recursive_most_common(binaries_array)
+  end
+
+  def calculate_co2_scrubber(binaries_array)
+    recursive_least_common(binaries_array)
+  end
+
+  def recursive_most_common(set, position: 0)
+    one_subset = []
+    zero_subset = []
+    positional_sum = 0
+
+    set.each do |binary_array|
+      one_subset << binary_array if binary_array[position] == 1
+      zero_subset << binary_array if binary_array[position] == 0
+
+      positional_sum += binary_array[position]
+    end
+
+    one_is_most_common = positional_sum >= (set.length / 2.0)
+
+    subset = one_is_most_common ? one_subset : zero_subset
+
+    if subset.length == 1
+      subset.flatten
+    else
+      recursive_most_common(subset, position: (position + 1))
+    end
+  end
+
+  def recursive_least_common(set, position: 0)
+    one_subset = []
+    zero_subset = []
+    positional_sum = 0
+
+    set.each do |binary_array|
+      one_subset << binary_array if binary_array[position] == 1
+      zero_subset << binary_array if binary_array[position] == 0
+
+      positional_sum += binary_array[position]
+    end
+
+    zero_is_least_common = positional_sum >= (set.length / 2.0)
+
+    subset = zero_is_least_common ? zero_subset : one_subset
+
+    if subset.length == 1
+      subset.flatten
+    else
+      recursive_least_common(subset, position: (position + 1))
+    end
   end
 end
--- spec/day_3_spec.rb
+++ spec/day_3_spec.rb
@@ -19,6 +19,38 @@ describe Day3 do
     end
   end
 
+  context "part 2" do
+    context "sample input" do
+      it "works" do
+        runner = Day3.new(sample_input_path)
+
+        expect(runner.part2).to eq("10111")
+      end
+    end
+
+    context "puzzle input" do
+      it "works" do
+      end
+    end
+  end
+
+  def test_input
+    %w[
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+    ].map { |binary| binary.chars.map(&:to_i) }
+  end
+
   def sample_input_path
     "./spec/support/fixtures/day_3_sample.txt"
   end
